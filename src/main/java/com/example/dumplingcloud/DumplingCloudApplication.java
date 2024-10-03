d25376de75fba3983cd6a9506fd272cb74f73eec
Irinaa0526 <irina.chernecova.00@mail.ru>
2024-10-04 01:11:45 +0300

    Initial commit

---
+++ src/main/java/com/example/dumplingcloud/DumplingCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.dumplingcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumplingCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DumplingCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dumplingcloud/DumplingCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dumplingcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DumplingCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
